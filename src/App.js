065428429f75e6660352a920ebf6b84b629547a1
igalbo <68712178+igalbo@users.noreply.github.com>
2022-05-26 04:31:19 +0300

    fixing browsing2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ViewProvider } from "./ViewContext";
 import RealEstate from "./pages/RealEstate";
 import Map from "./pages/Map";
